1c60f8efbf20cebdb982f603d0219a78569aa236
Mark Carver <mark.carver@me.com>
2017-05-27 15:26:38 -0500

    Issue #2509946 by markcarver: Provide methods/alters for supplying alphanumeric characters

---
+++ alpha_pagination.api.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @file
+ * The API for the Views Alpha Pagination module.
+ */
+
+/**
+ * Allows modules and themes to alter the alphabets array prior to rendering.
+ *
+ * Note: Do not use range(); always be explicit when defining an alphabet.
+ * This is necessary as you cannot rely on the server language to construct
+ * proper alphabet characters.
+ *
+ * @param array $alphabets
+ *   An associative array of key/value pairs, passed by reference. Each key is
+ *   a corresponding langcode and its value is an indexed array of characters
+ *   for that langcode.
+ * @param \alpha_pagination_handler_pagination $view
+ *   The View instance.
+ *
+ * @see \alpha_pagination_handler_pagination::getAlphabet()
+ */
+function hook_alpha_pagination_alphabet_alter(array &$alphabets, \alpha_pagination_handler_pagination $view) {
+  // Remove Z from the list.
+  array_pop($alphabets['en']);
+}
+
+/**
+ * Allows modules and themes to alter the numbers array prior to rendering.
+ *
+ * Note: Do not use range(); always be explicit when defining numbers.
+ * This is necessary as you cannot rely on the server language to construct
+ * proper numerical characters.
+ *
+ * @param array $numbers
+ *   An associative array of key/value pairs, passed by reference. Each key is
+ *   a corresponding langcode and its value is an indexed array of numbers
+ *   for that langcode.
+ * @param \alpha_pagination_handler_pagination $view
+ *   The View instance.
+ *
+ * @see \alpha_pagination_handler_pagination::getNumbers()
+ */
+function hook_alpha_pagination_numbers_alter(array &$numbers, \alpha_pagination_handler_pagination $view) {
+  // Remove 0 from the list.
+  array_shift($numbers['en']);
+}
